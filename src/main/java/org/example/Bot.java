490be05d2a5296532e2bc64247c06c7b9ea68cdb
IRQ3 <69483036+IRQ33@users.noreply.github.com>
2024-11-11 22:07:05 +0100

    update

--- src/main/java/org/example/Bot.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public class Bot {
-    public static String TOKEN = "MTMwMTI3NzI2MTk3Mzk0NjQwOQ.GH0Uad.BUbmZOBPM8Zdv_3F0s7ih8RuqnBtboDZgBNI0";
-
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import com.google.gson.Gson;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -14,11 +13,16 @@ import org.example.Listeners.WelcomeUser;
 import java.io.*;
 import java.nio.file.Path;
 import java.util.EnumSet;
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Properties properties=  new Properties();
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("config.properties");
+        properties.load(inputStream);
+        String token = properties.getProperty("TOKEN");
 
-        JDA jda =JDABuilder.createLight(Bot.TOKEN,EnumSet.of(GatewayIntent.GUILD_MEMBERS,GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT,GatewayIntent.GUILD_PRESENCES))
+        JDA jda =JDABuilder.createLight(token,EnumSet.of(GatewayIntent.GUILD_MEMBERS,GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT,GatewayIntent.GUILD_PRESENCES))
                 .enableCache(CacheFlag.ONLINE_STATUS).addEventListeners(new Welcome(), new WelcomeUser()).build();
 
         CommandManager commandManager = new CommandManager();
